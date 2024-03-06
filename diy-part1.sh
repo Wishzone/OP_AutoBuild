488cc27e9cec31f6a9cb5281c9575d50208abb03
Wishzone <89642468+Wishzone@users.noreply.github.com>
2024-03-06 15:15:48 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -12,6 +12,7 @@
 
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+git clone https://github.com/jerrykuku/luci-theme-argon.git package/
 
 # Add a feed source
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
